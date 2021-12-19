e2aeaa916f2a94c74d12807f8d66df082755f93d
Enes Can UYAR <uyar.enescan@gmail.com>
2021-12-19 16:21:58 +0300

    Create book object

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿namespace EnesCanUyar_Odev1_BooksApi
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string KitapSeriNo { get; set; }
+        public string KitapAdi { get; set; }
+        public string Yazari { get; set; }
+        public int SayfaSayisi { get; set; }
+
+        public Book(int _id, string _kitapSeriNo, string _kitapAdi, string _yazari, int _sayfaSayisi)
+        {
+            Id = _id;
+            KitapSeriNo = _kitapSeriNo;
+            KitapAdi = _kitapAdi;
+            Yazari = _yazari;
+            SayfaSayisi = _sayfaSayisi;
+        }
+    }
+}
