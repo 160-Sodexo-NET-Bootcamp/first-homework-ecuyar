6a22d7b972b2cdd70afca8edf6a7d2e8ee6c5bae
Enes Can UYAR <uyar.enescan@gmail.com>
2021-12-19 15:31:39 +0300

    Initial commit, delete examples

---
+++ Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnesCanUyar_Odev1_BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+}
