6cd887d1525505c132891a07b4c4d8f20384a50a
Enes Can UYAR <uyar.enescan@gmail.com>
2021-12-19 15:25:35 +0300

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace EnesCanUyar_Odev1_BooksApi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
